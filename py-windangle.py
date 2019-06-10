d83430f8c6f12caf681da596081f2637dc46388a
Jorge <tetris.attack@hotmail.com>
2019-06-09 23:06:05 -0300

    0.82 avg 2.3 max 28.64 total

--- py-windangle.py
+++ py-windangle.py
@@ -8,7 +8,7 @@ import numpy
 import glob
 
 # globais
-glob_printstep = False   # mostra os passos de processamento foram executados para cada imagem
+glob_printstep = False  # print o nome de cada imagem
 glob_printresult = False  # mostra os resultados para cada imagem
 
 # variáveis
@@ -53,68 +53,56 @@ for img_path in img_list:
     # extrai um quadrado de 68x68 no canto inferior direito - funciona para todas as resoluções
     crop_img = img[img_h-91:img_h-91+68, img_w-83:img_w-83+68]
     cv2.imwrite(crop_path, crop_img)
-    printstep("executado passo 1: crop")
     
     # aplica máscara
     mask_img = cv2.imread('~mask.png', 0)
     proc_img = cv2.bitwise_and(crop_img, crop_img, mask=mask_img)
     color_value = proc_img[26,64]
     proc_img[numpy.where((proc_img==[0,0,0]).all(axis=2))] = color_value
-    cv2.imwrite(output_imgpath + "1_mask.png", proc_img)
-    printstep("executado passo 1: máscara")
-    
-    # inpainting
-    inmask_img = cv2.imread('~inmask.png', 0)
-    proc_img = cv2.inpaint(proc_img, inmask_img, 1, cv2.INPAINT_NS)
-    cv2.imwrite(output_imgpath + "2_inpaint.png", proc_img)
-    printstep("executado passo 2: inpainting")
-    
+    cv2.imwrite(output_imgpath + "2_mask.png", proc_img)
+
+
     # converte para outro espaço de cores
     proc_img = cv2.cvtColor(proc_img, cv2.COLOR_BGR2Lab)
     proc_img = proc_img[:,:,2]  # extrai o canal
     cv2.imwrite(output_imgpath + "3_colorspace.png", proc_img)
-    printstep("executado passo 2: conversão para outro espaço de cor")
-    
+
+
     # inpainting
     inmask_img = cv2.imread('~inmask.png', 0)
     proc_img = cv2.inpaint(proc_img, inmask_img, 1, cv2.INPAINT_NS)
     cv2.imwrite(output_imgpath + "4_inpaint.png", proc_img)
-    printstep("executado passo 2: inpainting")
-    
+
+
     # normaliza com CLAHE para melhorar o resultado do inpainting
     clahe = cv2.createCLAHE(clipLimit=1.0, tileGridSize=(1,1))
     proc_img = clahe.apply(proc_img)
-    cv2.imwrite(output_imgpath + "5_dclahe.png", proc_img)
-    printstep("executado passo 2: CLAHE")
-    
+    cv2.imwrite(output_imgpath + "5_clahe.png", proc_img)
+
+
     # binariza a imagem usando otsu
     threshold_value, proc_img = cv2.threshold(proc_img, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
     cv2.imwrite(output_imgpath + "6_binary.png", proc_img)
-    printstep("executado passo 3: binarização")
 
     
     # aplica morfologia
     kernel = numpy.ones((3,3), numpy.uint8)
     proc_img = cv2.morphologyEx(proc_img, cv2.MORPH_OPEN, kernel, iterations=2)
     cv2.imwrite(output_imgpath + "7_morph.png", proc_img)
-    printstep("executado passo 5: abertura")
-    
-    # detecta contorno
+
+
+    # detecta e desenha o contorno, apenas para fins de visualização
     contours, hierarchy = cv2.findContours(proc_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    # desenha o contorno, apenas para fins de visualização
     contorno_img = cv2.imread(crop_path)
     cv2.drawContours(contorno_img, contours, -1, (0,255,0), 1)
     cv2.imwrite(output_imgpath + "8_contorno.png", contorno_img)
-    printstep("executado passo 6: detectado contorno")
     
     
-    # desenha a elipse
+    # desenha a elipse, apenas para fins de visualização
     contours = contours[0]
     ellipse = cv2.fitEllipse(contours)
-    # desenha a elipse, apenas para fins de visualização
     cv2.ellipse(crop_img, ellipse, (0,255,0), 1)
     cv2.imwrite(output_imgpath + "9_elipse.png", crop_img)
-    printstep("executado passo 7: traçada elipse")
 
 
     # análise dos resultados
