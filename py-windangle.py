62aa2d4a3b29ddfdc474c7b408b2725bc3fa2c1c
Jorge <tetris.attack@hotmail.com>
2019-06-09 22:50:00 -0300

    0.82 avg 2.3 max 28.64 total

--- py-windangle.py
+++ py-windangle.py
@@ -4,18 +4,21 @@
 
 # imports
 import cv2
-import numpy as np
+import numpy
 import glob
 
 # globais
-glob_printstep = False    # mostra os passos de processamento foram executados para cada imagem
-glob_printresult = True  # mostra os resultados para cada imagem
+glob_printstep = False   # mostra os passos de processamento foram executados para cada imagem
+glob_printresult = False  # mostra os resultados para cada imagem
 
 # variáveis
 input_dir = ""
 output_dir = "out/"
 avg_error = 0
 max_error = 0
+img_maxerr = ""
+quad_imgs = [0] * 4  # armazena número de imagens por quadrante
+quad_err = [0] * 4   # armazena erro total por quadrante
 
 
 # funções helper para prints
@@ -30,8 +33,8 @@ def printresult(string):
 
 # lista de nomes de arquivos a avaliar
 img_list = glob.glob("*.jpg")
-print(img_list)
-print()
+# print(img_list)
+# print()
 
 # loop que trabalha a lista de imagens
 for img_path in img_list:
@@ -51,53 +54,57 @@ for img_path in img_list:
     crop_img = img[img_h-91:img_h-91+68, img_w-83:img_w-83+68]
     cv2.imwrite(crop_path, crop_img)
     printstep("executado passo 1: crop")
-
-
-    # converte para outro espaço de cores
-    colorspace_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2Lab)
-    colorspace_img = colorspace_img[:,:,2]  # extrai o canal
-    cv2.imwrite(output_imgpath + "2_colorspace.png", colorspace_img)
-    printstep("executado passo 2: conversão para outro espaço de cor")
-
-    # teste inpainting
-    inmask_img = cv2.imread('~inmask.png', 0)
-    colorspace_img = cv2.inpaint(colorspace_img, inmask_img, 2, cv2.INPAINT_NS)
-    cv2.imwrite(output_imgpath + "2_inpaint.png", colorspace_img)
-    printstep("executado passo 6: inpainting")
-    
-    # binariza a imagem usando otsu
-    threshold_value, binary_img = cv2.threshold(colorspace_img, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-    cv2.imwrite(output_imgpath + "3_binary.png", binary_img)
-    printstep("executado passo 3: binarização")
-    
     
     # aplica máscara
     mask_img = cv2.imread('~mask.png', 0)
-    mask_img = cv2.bitwise_and(binary_img, binary_img, mask=mask_img)
-    cv2.imwrite(output_imgpath + "4_mask.png", mask_img)
-    printstep("executado passo 4: máscara")
+    proc_img = cv2.bitwise_and(crop_img, crop_img, mask=mask_img)
+    color_value = proc_img[26,64]
+    proc_img[numpy.where((proc_img==[0,0,0]).all(axis=2))] = color_value
+    cv2.imwrite(output_imgpath + "1_mask.png", proc_img)
+    printstep("executado passo 1: máscara")
     
+    # inpainting
+    inmask_img = cv2.imread('~inmask.png', 0)
+    proc_img = cv2.inpaint(proc_img, inmask_img, 1, cv2.INPAINT_NS)
+    cv2.imwrite(output_imgpath + "2_inpaint.png", proc_img)
+    printstep("executado passo 2: inpainting")
+    
+    # converte para outro espaço de cores
+    proc_img = cv2.cvtColor(proc_img, cv2.COLOR_BGR2Lab)
+    proc_img = proc_img[:,:,2]  # extrai o canal
+    cv2.imwrite(output_imgpath + "3_colorspace.png", proc_img)
+    printstep("executado passo 2: conversão para outro espaço de cor")
+    
+    # inpainting
+    inmask_img = cv2.imread('~inmask.png', 0)
+    proc_img = cv2.inpaint(proc_img, inmask_img, 1, cv2.INPAINT_NS)
+    cv2.imwrite(output_imgpath + "4_inpaint.png", proc_img)
+    printstep("executado passo 2: inpainting")
+    
+    # normaliza com CLAHE para melhorar o resultado do inpainting
+    clahe = cv2.createCLAHE(clipLimit=1.0, tileGridSize=(1,1))
+    proc_img = clahe.apply(proc_img)
+    cv2.imwrite(output_imgpath + "5_dclahe.png", proc_img)
+    printstep("executado passo 2: CLAHE")
+    
+    # binariza a imagem usando otsu
+    threshold_value, proc_img = cv2.threshold(proc_img, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    cv2.imwrite(output_imgpath + "6_binary.png", proc_img)
+    printstep("executado passo 3: binarização")
+
     
     # aplica morfologia
-    kernel = np.ones((3,3), np.uint8)
-    morph_img = cv2.morphologyEx(mask_img, cv2.MORPH_OPEN, kernel, iterations=2)
-    cv2.imwrite(output_imgpath + "5_morph.png", morph_img)
+    kernel = numpy.ones((3,3), numpy.uint8)
+    proc_img = cv2.morphologyEx(proc_img, cv2.MORPH_OPEN, kernel, iterations=2)
+    cv2.imwrite(output_imgpath + "7_morph.png", proc_img)
     printstep("executado passo 5: abertura")
     
-    '''
-    # teste inpainting
-    inmask_img = cv2.imread('~inmask.png', 0)
-    morph_img = cv2.inpaint(morph_img, inmask_img, 2, cv2.INPAINT_NS)
-    cv2.imwrite(output_imgpath + "6_inpaint.png", morph_img)
-    printstep("executado passo 6: inpainting")
-    '''
-
     # detecta contorno
-    contours, hierarchy = cv2.findContours(morph_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    contours, hierarchy = cv2.findContours(proc_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     # desenha o contorno, apenas para fins de visualização
     contorno_img = cv2.imread(crop_path)
     cv2.drawContours(contorno_img, contours, -1, (0,255,0), 1)
-    cv2.imwrite(output_imgpath + "6_contorno.png", contorno_img)
+    cv2.imwrite(output_imgpath + "8_contorno.png", contorno_img)
     printstep("executado passo 6: detectado contorno")
     
     
@@ -105,35 +112,40 @@ for img_path in img_list:
     contours = contours[0]
     ellipse = cv2.fitEllipse(contours)
     # desenha a elipse, apenas para fins de visualização
-    cv2.ellipse(crop_img,ellipse,(0,255,0),1)
-    cv2.imwrite(output_imgpath + "7_elipse.png", crop_img)
+    cv2.ellipse(crop_img, ellipse, (0,255,0), 1)
+    cv2.imwrite(output_imgpath + "9_elipse.png", crop_img)
     printstep("executado passo 7: traçada elipse")
-    
-    
+
+
     # análise dos resultados
     printresult("calculando %s..." % img_name)
     ang_obtido = ellipse[2]
     if(ang_obtido > 90):
         ang_obtido = 180 - ang_obtido
-    
+
     # ang_esperado apenas deve ser usado para as imagens de debugging
-    ang_esperado = int(img_name.split("_")[1])
+    ang_esperado = int(img_name.split("_")[0])
     ang_esperado /= 10
     error = abs(ang_esperado - ang_obtido)
     printresult("ang esp: %.1f" % ang_esperado)
     printresult("ang obt: %.1f" % ang_obtido)
-    
+
     # mais lógica de debugging
     printresult("erro: %.1f" % error)
     avg_error += error
+    quad_imgs[int(img_name.split("_")[2])-1] += 1
+    quad_err[int(img_name.split("_")[2])-1] += error
     if(error > max_error):
         max_error = error
-    
+        img_maxerr = img_name
+
     if(glob_printstep or glob_printresult): # imprime uma newline entre cada iteração do loop se estiver debugando
         print()
-    
+
 
 # saída do loop. print dos resultados finais
-print("Analisadas %d imagens..." % len(img_list))
-print("Erro médio: %.1f" % float(avg_error/len(img_list)))
-print("Maior erro: %.1f" % max_error)
+print("Analisadas %d imagens... [%d, %d, %d, %d]" % (len(img_list), quad_imgs[0], quad_imgs[1], quad_imgs[2], quad_imgs[3]))
+
+print("Erro total/médio: %.2f, %.2f" % (avg_error, float(avg_error/len(img_list))))
+print("Erro médio por quadrante: [%.2f], [%.2f], [%.2f], [%.2f]" % (float(quad_err[0]/quad_imgs[0]), float(quad_err[1]/quad_imgs[1]), float(quad_err[2]/quad_imgs[2]), float(quad_err[3]/quad_imgs[3])))
+print("Maior erro: %.1f, %s" % (max_error, img_maxerr))
